2d71434c1245639d7f870250e84d327ea01486f5
Andrew Marshall <andydanger12@outlook.com>
2023-06-12 11:51:56 -0500

    Replace Twitter and Discord links with LinkedIn link.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import './styles/footerStyle.css'
 
-import { ReactComponent as DiscordSVG } from '../../assets/images/discord.svg';
 import { ReactComponent as GitHubLogoSVG } from '../../assets/images/github.svg';
-import { ReactComponent as TwitterSVG } from '../../assets/images/twitter.svg';
+import { ReactComponent as LinkedInSVG } from '../../assets/images/linkedin.svg';
 
 export function Footer() {
 
@@ -12,11 +11,8 @@ export function Footer() {
             <a target="_blank" rel="noreferrer" href="https://github.com/AndyDanger/andydanger.github.io">
                 <GitHubLogoSVG className="footer-img" />
             </a>
-            <a target="_blank" rel="noreferrer" href="https://twitter.com/andydangerzone">
-                <TwitterSVG className="footer-img" />
-            </a>
-            <a target="_blank" rel="noreferrer" href="https://discord.com/users/183408194209579008">
-                <DiscordSVG className="footer-img" />
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/andrew-marshall-32b404110/">
+                <LinkedInSVG className="footer-img" />
             </a>
         </nav>
     );
