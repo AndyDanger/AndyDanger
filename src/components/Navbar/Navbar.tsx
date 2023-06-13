92f51889a432237b5af30d7a2cfac0f782308882
Andrew Marshall <andydanger12@outlook.com>
2023-06-12 19:06:33 -0500

    Adjust navigation spacing and make logo clickable.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import './styles/navbarStyle.css'
-import { ReactComponent as LoLLogoSVG } from '../../assets/images/league-of-legends.svg';
 import { ReactComponent as LogoSVG } from '../../assets/images/logo.svg';
 
-import { Link } from "react-router-dom";
 import { ThemeToggler } from "./ThemeToggler";
 
 export function Navbar() {
     return (
         <nav className="navbar-container">
             <div className="navbar-logo-container">
-                <Link className="navbar-logo" to="/">
-                    <LogoSVG />
-                </Link>
+                <div className="navbar-logo">
+                    <LogoSVG className="navbar-icon" />
+                </div>
                 <div className="slider-nav">
                     <h2 className="navbar-icon">Home</h2>
                     <h2 className="navbar-icon">Work</h2>
